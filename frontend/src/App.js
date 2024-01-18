c5415ca0248da770ec3a5da25f1489ee00c804bc
moheet333 <moheetshendarkar@gmail.com>
2024-01-19 02:59:28 +0530

    UPDATE: worked on chat engine

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -24,34 +24,35 @@ import DashboardB from "./Pages/Login/dashboard for user/DashboardB.js";
 import Complaint from "./Pages/Login/dashboard for user/CaseListP.js";
 import CaseListP from "./Pages/Login/dashboard for user/CaseListP.js";
 import Chat from "./Components/Chat.js";
-
+import ChatEngineComponent from "./Pages/Login/ChatEngine.js";
 
 function App() {
-    return (
-        <UserProvider>
-            <CompanyProvider>
-                <BrowserRouter>
-                    <Navbar />
-                    <Routes>
-                        <Route path="/login" element={<Login />} />
-                        <Route path="/loginb" element={<LoginB />} />
-                        <Route path="/dashboardbusiness" element={<Dashboard />} />
-                        <Route path="/" element={<LP />} />
-                        <Route path="/docs" element={<Documents />} />
-                        <Route path="/help" element={<Help />} />
-                        <Route path="/case" element={<DisplayCaseDetailsPage />} />
-                        <Route path="/thanks" element={<Thanks />} />
-                        <Route path="/dashboarduser" element={<Dashboarduser />} />
-                        <Route path="/dashb" element={<DashboardB />} />
-                        <Route path="/ask" element={<Ask />} />
-                        <Route path="/caselists" element={<CaseListP />} />
-                        <Route path="/chat" element={<Chat />} />
-                    </Routes>
-                    <Footer />
-                </BrowserRouter>
-            </CompanyProvider>
-        </UserProvider>
-    );
+  return (
+    <UserProvider>
+      <CompanyProvider>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/loginb" element={<LoginB />} />
+            <Route path="/dashboardbusiness" element={<Dashboard />} />
+            <Route path="/" element={<LP />} />
+            <Route path="/docs" element={<Documents />} />
+            <Route path="/help" element={<Help />} />
+            <Route path="/case" element={<DisplayCaseDetailsPage />} />
+            <Route path="/thanks" element={<Thanks />} />
+            <Route path="/dashboarduser" element={<Dashboarduser />} />
+            <Route path="/dashb" element={<DashboardB />} />
+            <Route path="/ask" element={<Ask />} />
+            <Route path="/caselists" element={<CaseListP />} />
+            <Route path="/chat" element={<Chat />} />
+            <Route path="/chatengine" element={<ChatEngineComponent />} />
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+      </CompanyProvider>
+    </UserProvider>
+  );
 }
 
 export default App;
