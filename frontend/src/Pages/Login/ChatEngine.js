d7fbc67a974239787043d2b941f4a3b4c2fbcfbb
Sarahkhan20 <sarahejaz77@gmail.com>
2024-01-19 04:19:33 +0530

    we we

--- frontend/src/Pages/Login/ChatEngine.js
+++ frontend/src/Pages/Login/ChatEngine.js
@@ -1,15 +1,15 @@
 import { ChatEngine } from "react-chat-engine";
 
 const ChatEngineComponent = () => {
-  return (
-    <div>
-      <ChatEngine
-        projectID="3a259a8c-d0bf-4004-b787-7d5833ba50f5"
-        userName="moheet"
-        userSecret="moheet"
-      />
-    </div>
-  );
+    return (
+        <div>
+            <ChatEngine
+                projectID="3a259a8c-d0bf-4004-b787-7d5833ba50f5"
+                userName="moheet"
+                userSecret="moheet"
+            />
+        </div>
+    );
 };
 
 export default ChatEngineComponent;
